41f380a5089044d20faea67493da89c1cff49b0d
jb0gie <wxzvrd@protonmail.com>
2024-06-16 15:58:12 +0100

    hmmm

---
+++ apps/boltevm/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function BoltApp() {
+  return (
+    <app>
+      <rigidbody type="kinematic">
+        <model src="bolt_base.glb" />
+      </rigidbody>
+    </app>
+  )
+}
